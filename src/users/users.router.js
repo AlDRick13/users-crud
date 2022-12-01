df27a47b4e992010ed9a0c6ad3ec620ad9647656
Aldo Ricardo Reygadas Hernandez <aldo@hobbio.co>
2022-11-30 20:06:40 -0500

    v2

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -5,7 +5,7 @@ const userServices = require('./users.services');
 router.get('/user', userServices.getAllUsers);
 router.post('/user', userServices.postUser);
 router.get('/user/:id', userServices.getUserById);
-router.patch('/user', userServices.patchUser);
+router.patch('/user/:id', userServices.patchUser);
 router.delete('/user/:id', userServices.deleteUser);
 
 module.exports = router;
